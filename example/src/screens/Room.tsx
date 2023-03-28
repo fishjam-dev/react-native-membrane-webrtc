48b62c9ccb59517d4abd830fdfc8a11b305233cb
skyman503 <karolsygiet@gmail.com>
2023-03-28 08:33:55 +0200

    FIx layout

--- example/src/screens/Room.tsx
+++ example/src/screens/Room.tsx
@@ -6,9 +6,15 @@ import { Typo } from '@components/Typo';
 import * as Membrane from '@jellyfish-dev/react-native-membrane-webrtc';
 import { RootStack } from '@model/NavigationTypes';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
+<<<<<<< HEAD
 import React, { useCallback, useEffect } from 'react';
 import { StyleSheet, View, Pressable, Dimensions } from 'react-native';
 import { useVideoroomState } from 'src/VideoroomContext';
+=======
+import { findIndex } from 'lodash';
+import React, { useCallback, useEffect, useState, useRef } from 'react';
+import { StyleSheet, View, Pressable, Dimensions } from 'react-native';
+>>>>>>> f985a87 (FIx layout)
 
 import { CallControls } from '../components/CallControls';
 
@@ -21,6 +27,7 @@ type Props = NativeStackScreenProps<RootStack, 'Room'>;
 
 export const Room = ({ navigation }: Props) => {
   const { width, height } = Dimensions.get('window');
+<<<<<<< HEAD
   const { roomName } = useVideoroomState();
   const participants = Membrane.useRoomParticipants();
   console.log(participants);
@@ -31,10 +38,19 @@ export const Room = ({ navigation }: Props) => {
       HEADER_AND_FOOTER_SIZE -
       PADDING_BETWEEN_PARTICIPANTS * (participants.length / 2 + 2)) /
     Math.ceil(participants.length / 2);
+=======
+  const videoViewWidth = (width - 32 - 16) / 2;
+
+  const participants = Membrane.useRoomParticipants();
+  const [currentCamera, setCurrentCamera] =
+    useState<Membrane.CaptureDevice | null>(null);
+  const availableCameras = useRef<Membrane.CaptureDevice[]>([]);
+>>>>>>> f985a87 (FIx layout)
 
   const { disconnect: mbDisconnect } = Membrane.useMembraneServer();
 
   useEffect(() => {
+<<<<<<< HEAD
     const handleBeforeRemoveEvent = (e) => {
       e.preventDefault();
       // Check whether beforeRemove event was triggered by disconenct button.
@@ -48,6 +64,13 @@ export const Room = ({ navigation }: Props) => {
     return () =>
       navigation.removeListener('beforeRemove', handleBeforeRemoveEvent);
   }, [navigation]);
+=======
+    Membrane.getCaptureDevices().then((devices) => {
+      availableCameras.current = devices;
+      setCurrentCamera(devices.find((device) => device.isFrontFacing) || null);
+    });
+  }, []);
+>>>>>>> f985a87 (FIx layout)
 
   const disconnect = useCallback(() => {
     mbDisconnect();
@@ -158,6 +181,19 @@ const styles = StyleSheet.create({
   },
   participantsContainer: {
     flex: 1,
+<<<<<<< HEAD
+=======
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: '#001A72',
+    margin: 20,
+  },
+  focusedParticipant: {
+    flex: 1,
+  },
+  otherParticipantsContainer: {
+    flex: 1,
+>>>>>>> f985a87 (FIx layout)
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 16,
@@ -183,5 +219,43 @@ const styles = StyleSheet.create({
     marginBottom: 8,
     marginLeft: 4,
     marginRight: 4,
+<<<<<<< HEAD
+=======
+  },
+  displayNameContainer: {
+    backgroundColor: BrandColors.darkBlue80,
+    borderRadius: 60,
+    position: 'absolute',
+    left: 16,
+    bottom: 16,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  displayName: {
+    paddingLeft: 12,
+    paddingRight: 12,
+    paddingTop: 6,
+    paddingBottom: 5,
+  },
+  settingsButton: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+  },
+  settingsWrapper: {
+    position: 'absolute',
+    right: 4,
+    bottom: 48,
+  },
+  disabledIconsContainer: {
+    flexDirection: 'row',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+  },
+  vadSpeech: {
+    borderWidth: 5,
+    borderColor: BrandColors.green60,
+>>>>>>> f985a87 (FIx layout)
   },
 });
