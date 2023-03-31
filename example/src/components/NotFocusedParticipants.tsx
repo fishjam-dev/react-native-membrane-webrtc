31038769b29e29f2e85dde8999b147ae0a32a67e
Karol Sygiet <karolsygiet@gmail.com>
2023-03-31 15:00:47 +0200

    [MV-346] Add pin functionality (#76)
    Adds pin/unpin button with custom animations, that can be used to pin/unpin video track during a call.

---
+++ example/src/components/NotFocusedParticipants.tsx
@@ -0,0 +1,96 @@
+import { BrandColors } from '@colors';
+import * as Membrane from '@jellyfish-dev/react-native-membrane-webrtc';
+import { isEmpty } from 'lodash';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+import { OtherParticipants } from './OtherParticipants';
+import { RoomParticipant } from './RoomParticipant';
+
+type NotFocusedParticipantsProp = { participants: Membrane.Participant[] };
+
+export const NotFocusedParticipants = ({
+  participants,
+}: NotFocusedParticipantsProp) => {
+  if (isEmpty(participants)) {
+    return null;
+  }
+
+  return (
+    <View style={styles.container}>
+      {participants.length === 1 ? (
+        <View style={styles.otherParticipantContainer}>
+          <RoomParticipant
+            participant={participants[0]}
+            pinButtonHiddden
+            tileSmall
+          />
+        </View>
+      ) : (
+        <View
+          style={[
+            styles.otherParticipantsContainer,
+            styles.otherParticipantContainer,
+          ]}
+        >
+          <View style={styles.participant}>
+            <RoomParticipant
+              participant={participants[0]}
+              pinButtonHiddden
+              tileSmall
+            />
+          </View>
+          <View style={styles.middleLine} />
+          <View style={styles.participant}>
+            {participants.length === 2 ? (
+              <RoomParticipant
+                participant={participants[1]}
+                pinButtonHiddden
+                tileSmall
+              />
+            ) : (
+              <OtherParticipants
+                p1={participants[1]}
+                p2={participants[2]}
+                numOfOtherParticipants={participants.length - 1}
+              />
+            )}
+          </View>
+        </View>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  otherParticipantContainer: {
+    borderRadius: 12,
+    borderWidth: 2,
+    borderColor: BrandColors.darkBlue60,
+    overflow: 'hidden',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  otherParticipantsContainer: {
+    flex: 1,
+    aspectRatio: 2,
+    flexDirection: 'row',
+    marginLeft: 32,
+    marginRight: 32,
+  },
+  participant: {
+    flex: 1,
+    aspectRatio: 1,
+    overflow: 'hidden',
+  },
+  middleLine: {
+    height: '100%',
+    width: 2,
+    backgroundColor: BrandColors.darkBlue60,
+  },
+});
--- example/src/screens/Room.tsx
+++ example/src/screens/Room.tsx
@@ -1,13 +1,14 @@
 import { BrandColors } from '@colors';
 import { BackgroundAnimation } from '@components/BackgroundAnimation';
 import { Icon } from '@components/Icon';
+import { NotFocusedParticipants } from '@components/NotFocusedParticipants';
 import { OtherParticipants } from '@components/OtherParticipants';
 import { RoomParticipant } from '@components/RoomParticipant';
 import { Typo } from '@components/Typo';
 import * as Membrane from '@jellyfish-dev/react-native-membrane-webrtc';
 import { RootStack } from '@model/NavigationTypes';
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { StyleSheet, View, Dimensions } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -25,7 +26,15 @@ type Props = NativeStackScreenProps<RootStack, 'Room'>;
 export const Room = ({ navigation }: Props) => {
   const { width, height } = Dimensions.get('window');
   const { roomName } = useVideoroomState();
+
   const participants = Membrane.useRoomParticipants();
+  const [focusedParticipantId, setFocusedParticipantId] = useState<
+    string | null
+  >(null);
+  const focusedParticipant = participants.find(
+    (p) => p.id === focusedParticipantId
+  );
+
   const rowNum = Math.min(
     Math.ceil(participants.length / 2),
     MAX_NUM_OF_USERS_ON_THE_SCREEN / 2
@@ -94,48 +103,76 @@ export const Room = ({ navigation }: Props) => {
             </View>
           </View>
 
-          <View style={styles.flex}>
-            <View style={styles.participantsContainer}>
-              <View
-                style={[
-                  styles.inner,
-                  participants.length > FLEX_BRAKPOINT
-                    ? styles.row
-                    : styles.column,
-                ]}
-              >
-                {participants
-                  .slice(
-                    0,
-                    participants.length > MAX_NUM_OF_USERS_ON_THE_SCREEN
-                      ? MAX_NUM_OF_USERS_ON_THE_SCREEN - 1
-                      : MAX_NUM_OF_USERS_ON_THE_SCREEN
-                  )
-                  .map((p) => (
-                    <View
-                      key={p.id}
-                      style={[
-                        getStylesForParticipants(participants),
-                        styles.shownParticipantBorder,
-                      ]}
-                    >
-                      <RoomParticipant participant={p} />
-                    </View>
-                  ))}
-
-                {participants.length > MAX_NUM_OF_USERS_ON_THE_SCREEN && (
-                  <View style={getStylesForParticipants(participants)}>
-                    <OtherParticipants
-                      p1={participants[participants.length - 1]}
-                      p2={participants[participants.length - 2]}
-                      numOfOtherParticipants={
-                        participants.length - MAX_NUM_OF_USERS_ON_THE_SCREEN + 1
-                      }
-                    />
-                  </View>
-                )}
+          <View style={{ flex: 1 }}>
+            {focusedParticipant ? (
+              <View style={styles.focusedParticipantContainer}>
+                <View style={styles.focusedParticipant}>
+                  <RoomParticipant
+                    participant={focusedParticipant}
+                    onPinButtonPressed={setFocusedParticipantId}
+                    focused
+                  />
+                </View>
               </View>
-            </View>
+            ) : null}
+
+            {focusedParticipant ? (
+              <View style={styles.otherParticipants}>
+                <NotFocusedParticipants
+                  participants={participants.filter(
+                    (p) => p.id !== focusedParticipant?.id
+                  )}
+                />
+              </View>
+            ) : (
+              <View style={styles.participantsContainer}>
+                <View
+                  style={[
+                    styles.inner,
+                    participants.length > FLEX_BRAKPOINT
+                      ? styles.row
+                      : styles.column,
+                  ]}
+                >
+                  {participants
+                    .slice(
+                      0,
+                      participants.length > MAX_NUM_OF_USERS_ON_THE_SCREEN
+                        ? MAX_NUM_OF_USERS_ON_THE_SCREEN - 1
+                        : MAX_NUM_OF_USERS_ON_THE_SCREEN
+                    )
+                    .map((p) => (
+                      <View
+                        key={p.id}
+                        style={[
+                          getStylesForParticipants(participants),
+                          styles.shownParticipantBorder,
+                        ]}
+                      >
+                        <RoomParticipant
+                          participant={p}
+                          onPinButtonPressed={setFocusedParticipantId}
+                          tileSmall={participants.length > FLEX_BRAKPOINT}
+                        />
+                      </View>
+                    ))}
+
+                  {participants.length > MAX_NUM_OF_USERS_ON_THE_SCREEN && (
+                    <View style={getStylesForParticipants(participants)}>
+                      <OtherParticipants
+                        p1={participants[participants.length - 1]}
+                        p2={participants[participants.length - 2]}
+                        numOfOtherParticipants={
+                          participants.length -
+                          MAX_NUM_OF_USERS_ON_THE_SCREEN +
+                          1
+                        }
+                      />
+                    </View>
+                  )}
+                </View>
+              </View>
+            )}
           </View>
           <CallControls />
         </SafeAreaView>
@@ -194,5 +231,26 @@ const styles = StyleSheet.create({
   shownParticipantBorder: {
     borderWidth: 1,
     borderColor: BrandColors.darkBlue60,
+  },
+  focusedParticipantContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: 16,
+    marginBottom: 16,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  focusedParticipant: {
+    aspectRatio: 1 / 1.3,
+    borderRadius: 12,
+    borderWidth: 1,
+    borderColor: BrandColors.darkBlue60,
+    overflow: 'hidden',
+    width: '100%',
+  },
+  otherParticipants: {
+    marginTop: 16,
+    marginBottom: 8,
+    flex: 1,
   },
 });
