89b85458fe7b65db53460ee30bcaf664aabece01
Angelika Serwa <angelika.serwa@swmansion.com>
2023-04-26 17:43:20 +0200

    downgrade to emulator api 28

--- example/e2e/avd-setup.sh
+++ example/e2e/avd-setup.sh
@@ -2,11 +2,11 @@
 sdkmanager --install emulator
 
 # Install an AOSP image that would later be used as the AVD's OS
-sdkmanager --install "system-images;android-29;default;x86_64"
+sdkmanager --install "system-images;android-28;default;x86_64"
 
 # To do that automatically in a script, try this:
 yes | sdkmanager --licenses
 
 # Create an AVD with the image we've previously installed
-avdmanager --verbose create avd --force --name Pixel_4_API_29 --abi x86_64 --device "pixel" --package "system-images;android-29;default;x86_64"
+avdmanager --verbose create avd --force --name Pixel_4_API_28 --abi x86_64 --device "pixel" --package "system-images;android-28;default;x86_64"
 
