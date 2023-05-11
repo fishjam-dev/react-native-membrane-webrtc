85306b59f87a63f0273c50aded6bb5d90bf8ea5c
skyman503 <karolsygiet@gmail.com>
2023-05-11 12:48:07 +0200

    add comment

--- src/index.tsx
+++ src/index.tsx
@@ -279,6 +279,18 @@ export enum LoggingSeverity {
   None = 'none',
 }
 
+/**
+ * A record of the total time, in seconds,
+ * that this stream has spent in each quality limitation state.
+ * none: The resolution and/or framerate is not limited.
+ * bandwidth: The resolution and/or framerate is primarily limited due to
+ * congestion cues during bandwidth estimation.
+ * Typical, congestion control algorithms use inter-arrival time,
+ * round-trip time, packet or other congestion cues to perform bandwidth estimation.
+ * cpu: The resolution and/or framerate is primarily limited due to CPU load.
+ * other: The resolution and/or framerate is primarily limited
+ * for a reason other than the above.
+ */
 export type QualityLimitationDurations = {
   bandwidth: number;
   cpu: number;
