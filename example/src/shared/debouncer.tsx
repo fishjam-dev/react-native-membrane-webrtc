c36c08431cd7664e663ee9bbcf49b4d0fd6f1dc1
Karol Sygiet <karolsygiet@gmail.com>
2023-05-30 17:06:33 +0200

    [MV-468] Fix standard button working multiple times when clicked very fast (#116)
    * init
    
    * Update example/src/shared/debouncer.tsx
    
    Co-authored-by: Angelika Serwa <angelika.serwa@swmansion.com>
    
    ---------
    
    Co-authored-by: Angelika Serwa <angelika.serwa@swmansion.com>

--- example/src/shared/debouncer.tsx
+++ example/src/shared/debouncer.tsx
@@ -1,12 +1,16 @@
 import { debounce } from 'lodash';
 import { useCallback } from 'react';
 
+// Make sure that the function used as a callback stays
+// always the same since making changes to it won't
+// affect the callback used in the useDebounce and the
+// original one will be used.
 export const useDebounce = (callback: Function) => {
   return useCallback(
     debounce(callback, 300, {
       leading: true,
       trailing: false,
     }),
-    [callback]
+    []
   );
 };
