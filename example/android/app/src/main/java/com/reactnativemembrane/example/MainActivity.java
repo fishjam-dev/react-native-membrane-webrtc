a1c1a767a80c1949dc8280a2e15b2765ca5c1a5f
Angelika Serwa <angelika.serwa@swmansion.com>
2023-07-11 16:47:22 +0200

    fixes

--- example/android/app/src/main/java/com/reactnativemembrane/example/MainActivity.java
+++ example/android/app/src/main/java/com/reactnativemembrane/example/MainActivity.java
@@ -1,4 +1,4 @@
-package com.reactnativemembrane.example;
+package com.membrane.reactnativemembrane;
 
 import android.graphics.Color;
 import android.os.Build;
--- example/android/app/src/main/java/com/reactnativemembrane/example/MainApplication.java
+++ example/android/app/src/main/java/com/reactnativemembrane/example/MainApplication.java
@@ -1,4 +1,4 @@
-package com.reactnativemembrane.example;
+package com.membrane.reactnativemembrane;
 
 import android.app.Application;
 import android.content.res.Configuration;
