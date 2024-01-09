e999259714607e8d894ad5fb03faba0179135463
Karol Kąkol <kakol.karol00@gmail.com>
2024-01-09 20:10:47 +0100

    serialize simulcast config v2

--- android/src/main/java/com/reactnativemembrane/SerializableTrackEncoding.kt
+++ android/src/main/java/com/reactnativemembrane/SerializableTrackEncoding.kt
@@ -8,37 +8,40 @@ enum class SerializableTrackEncoding(val rid: String) : Serializable {
   L("l"), M("m"), H("h");
 
   companion object {
-    fun fromTrackEncoding(encoding: TrackEncoding): SerializableTrackEncoding? {
-      return values().find { it.rid == encoding.rid }
+    fun fromTrackEncoding(encoding: TrackEncoding?): SerializableTrackEncoding? {
+      return values().find { it.rid == encoding?.rid }
     }
 
-    fun toTrackEncoding(encoding: SerializableTrackEncoding): TrackEncoding? {
-      return TrackEncoding.fromString(encoding.rid)
+    fun toTrackEncoding(encoding: SerializableTrackEncoding?): TrackEncoding? {
+      return encoding?.let {TrackEncoding.fromString(it.rid) }
     }
   }
 }
 
 data class SerializableSimulcastConfig(
   val enabled: Boolean = false,
-  val activeEncodings: List<SerializableTrackEncoding> = listOf()
+  val activeEncodings: List<SerializableTrackEncoding>? = listOf()
 ) : Serializable {
   companion object {
-    fun fromSimulcastConfig(config: SimulcastConfig): SerializableSimulcastConfig {
+    fun fromSimulcastConfig(config: SimulcastConfig?): SerializableSimulcastConfig? {
+      if(config == null) return null
       return SerializableSimulcastConfig(
         enabled = config.enabled,
         activeEncodings = config.activeEncodings.mapNotNull { SerializableTrackEncoding.fromTrackEncoding(it) }
       )
     }
 
-    fun toSimulcastConfig(config: SerializableSimulcastConfig): SimulcastConfig {
-      return SimulcastConfig(
-        enabled = config.enabled,
-        activeEncodings = config.activeEncodings.mapNotNull {
-          SerializableTrackEncoding.toTrackEncoding(
-            it
-          )
-        }
-      )
+    fun toSimulcastConfig(config: SerializableSimulcastConfig?): SimulcastConfig? {
+      return config?.activeEncodings?.let {
+        SimulcastConfig(
+          enabled = config.enabled,
+          activeEncodings = it.mapNotNull {
+            SerializableTrackEncoding.toTrackEncoding(
+              it
+            )
+          }
+        )
+      }
     }
   }
 }
